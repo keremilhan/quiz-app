3793399bba27c249961bbd465c2732ecc65d0097
Ali <ali.ibrahim.work@gmail.com>
2024-05-30 07:56:03 +0200

    chang the answer hey view
    Add toUpperCase

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -5,7 +5,7 @@
 export const createAnswerElement = (key, answerText) => {
   const element = document.createElement('li');
   element.innerHTML = String.raw`
-    ${key}: ${answerText}
+    ${key.toUpperCase()}: ${answerText}
   `;
   return element;
 };
